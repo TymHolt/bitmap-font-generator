07d65e781700f8423207c9b504f0810bf5eabd46
Tim Holtkötter <timholtkoetter.com@gmail.com>
2025-09-20 16:12:56 +0200

    PNG Export

--- src/org/bfg/form/implement/MainForm.java
+++ src/org/bfg/form/implement/MainForm.java
@@ -4,9 +4,13 @@ import org.bfg.form.base.InputForm;
 import org.bfg.generate.BitmapGenerationRunnable;
 import org.bfg.util.work.Work;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public final class MainForm extends InputForm {
 
@@ -14,6 +18,7 @@ public final class MainForm extends InputForm {
     private JComboBox<String> fieldFontStyle;
     private JSpinner fieldFontSize;
     private JButton buttonPreview;
+    private JButton buttonExport;
     private BufferedImage preview;
 
     @Override
@@ -24,6 +29,34 @@ public final class MainForm extends InputForm {
         this.fieldFontStyle = this.addSelectionInput("Font Style",
             new String[] {"Plain", "Bold", "Italic"});
         this.fieldFontSize = this.addNumberInput("Font Size", 10, 1, 100);
+        this.buttonExport = this.addAction("Export", actionEvent -> {
+            final JFileChooser fileChooser = new JFileChooser();
+            fileChooser.addChoosableFileFilter(new FileFilter() {
+
+                @Override
+                public boolean accept(File file) {
+                    return file.getName().endsWith(".png") || file.isDirectory();
+                }
+
+                @Override
+                public String getDescription() {
+                    return "Image (*.png)";
+                }
+            });
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            final int action = fileChooser.showDialog(this, "Export Image");
+
+            if (action == JFileChooser.APPROVE_OPTION) {
+                final File file = fileChooser.getSelectedFile();
+
+                try {
+                    ImageIO.write(this.preview, "PNG", file);
+                } catch (IOException exception) {
+                    JOptionPane.showMessageDialog(this, exception.getMessage(), "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
         this.buttonPreview = this.addAction("Preview", actionEvent -> {
             final PreviewForm form = new PreviewForm();
             form.setPreview(this.preview);
@@ -51,7 +84,9 @@ public final class MainForm extends InputForm {
 
     @Override
     public void onUpdate() {
-        SwingUtilities.invokeLater(() -> buttonPreview.setEnabled(this.preview != null));
+        final boolean previewGenerated = this.preview != null;
+        this.buttonPreview.setEnabled(previewGenerated);
+        this.buttonExport.setEnabled(previewGenerated);
     }
 
     private static int getStyleId(String name) {
